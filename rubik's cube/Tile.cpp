dff6f258d955d8d040dda8a3a34ec07591fd436c
ho-ody <r4f4l135@gmail.com>
2022-05-10 00:45:45 +0200

    klasa Tile z pozycjami i kolorami płytek

---
+++ rubik's cube/Tile.cpp
@@ -0,0 +1,6 @@
+#include "Tile.h"
+
+Tile::Tile(glm::vec3 position_in, glm::vec3 color_in) {
+	position = position_in;
+	color = color_in;
+}
---
+++ rubik's cube/Tile.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <glm/glm.hpp>
+
+class Tile
+{
+public:
+	glm::vec3 position; //relative to center
+	glm::vec3 color;
+	
+	Tile(glm::vec3, glm::vec3 = glm::vec3(0.3,0.3,0.3));
+};
