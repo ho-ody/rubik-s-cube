2ee75eccd371e97dd23fc59fd1f228060098be3a
ho-ody <r4f4l135@gmail.com>
2022-05-10 01:21:14 +0200

    Tile class: dodano rotację konkretnych tile'ów

--- rubik's cube/Tile.cpp
+++ rubik's cube/Tile.cpp
@@ -1,6 +1,7 @@
 #include "Tile.h"
 
-Tile::Tile(glm::vec3 position_in, glm::vec3 color_in) {
+Tile::Tile(glm::vec3 position_in, glm::vec3 color_in, glm::vec3 rotation_in) {
 	position = position_in;
 	color = color_in;
+	rotation = rotation_in;
 }
--- rubik's cube/Tile.h
+++ rubik's cube/Tile.h
@@ -6,6 +6,7 @@ class Tile
 public:
 	glm::vec3 position; //relative to center
 	glm::vec3 color;
+	glm::vec3 rotation;
 	
-	Tile(glm::vec3 = glm::vec3(0.0, 0.0, 0.0), glm::vec3 = glm::vec3(0.3,0.3,0.3));
+	Tile(glm::vec3 = glm::vec3(0.0, 0.0, 0.0), glm::vec3 = glm::vec3(0.3,0.3,0.3), glm::vec3 = glm::vec3(0.0, 0.0, 0.0));
 };
